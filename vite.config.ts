652c9f69dbdc76228b2618a77d7dc4e5ca3fdd02
Peiwen Lu <hi@peiwen.lu>
2023-10-21 21:07:47 +0800

    Init configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,44 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+import autoprefixer from "autoprefixer";
+
+let modulesConfig = {
+  generateScopedName: "[local]-[hash:base64:4]"
+};
+
+if (process.env.IS_PROD) {
+  const fileSet = {};
+  const hashSet = {};
+  modulesConfig = {
+    getJSON: function (file, json) {
+      if (fileSet[file]) return;
+
+      fileSet[file] = true;
+      Object.values(json).forEach((i) => {
+        if (hashSet[i]) throw Error("HASH COLLISION ERROR");
+        hashSet[i] = true;
+      });
+    },
+    generateScopedName: "[hash:base64:2]"
+  };
+}
 
 export default defineConfig({
   plugins: [solid()],
-})
+  vite: {
+    css: {
+      modules: modulesConfig,
+      postcss: {
+        plugins: [autoprefixer()]
+      },
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "~/styles/_common" as *;`
+        }
+      }
+    },
+    resolve: {
+      alias: [{ find: "~", replacement: "/src/" }]
+    }
+  }
+});
